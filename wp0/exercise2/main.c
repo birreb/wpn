367218eb2c03d0ba6bde840613bcf0cbe2f6b53b
Filip <carlfilip@hotmail.se>
2026-01-22 09:53:23 +0100

    WP0

--- wp0/exercise2/main.c
+++ wp0/exercise2/main.c
@@ -1,6 +1,6 @@
-#include <stdio.h>                                              //
+#include <stdio.h>                                              // Package for input-output functions
                                         
-int main (int argc, char **argv) {                              // We use ** because it points to an array of strings
-    printf("Hello World! – I'm %s!\n", argv[1]);                //
-    return 0;                                                   //
+int main (int argc, char *argv[]) {                              // We use ** because it points to an array of strings
+    printf("Hello World! – I'm %s!\n", argv[1]);                // Print a message with the name from command line arguments
+    return 0;                                                   // Return 0 to indicate successful execution
 }
--- wp0/exercise3/main.c
+++ wp0/exercise3/main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include <stdio.h>                                              // Package for input-output functions
+#include <string.h>                                             // Package for input-output functions       
 
-int main (int argc, char **argv) {
-    printf("Hello World! – I'm %s!\n", argv[1]);
-    return 0;
+int main (int argc, char *argv[]) {                             // Main function with command-line arguments    
+
+    if((argc) < 2){                                             // Check if no name is provided
+        printf("Error: No name provided.\n");                   // Print error message                                                      
+        printf("Use -h for help.\n");                           // Suggest help option
+        return 1;                                               // Return error code
+    }
+    else if((argc) > 2){                                        // Check if too many arguments are provided
+        printf("Error: Too many arguments provided.\n");        // Print error message      
+        printf("Use -h for help.\n");                           // Suggest help option
+        return 1;                                               // Return error code
+    }
+    else if(argc > 1 && strcmp(argv[1], "-h") == 0) {           // Check if help option is requested
+        printf("Usage: ./program_name [your_name]\n");          // Print usage instructions
+        return 1;                                               // Return error code
+    }
+    else {                                                      // If a name is provided
+        printf("Hello World! – I'm %s!\n", argv[1]);            // Print greeting message with the provided name
+        return 0;                                               // Return success code
+    }
 }
